0d7bb8ee0b97b1ee7bcd42a696869ff17276e131
Felipe Polo <felipe.polo@gmail.com>
2020-04-20 19:40:34 +0200

    Solving latest tests

--- bowling.test.js
+++ bowling.test.js
@@ -53,7 +53,7 @@ describe("bowling game", () => {
     describe("spare cases", () => {
         it('accounts for a spare frame', () => {
             forceSpare(2);
-            expect(bowling.getScore()).to.equal("frame: 2,/ -- total: 0");
+            expect(bowling.getScore()).to.equal("frame: 2,/ -- total: 10");
         });
         
         it('stops counting when a spare is reached', () => {
@@ -62,19 +62,19 @@ describe("bowling game", () => {
             bowling.roll(4);
             bowling.roll(5);
             forceSpare(6);
-            expect(bowling.getScore()).to.equal("frame: 6,/ -- total: 14");
+            expect(bowling.getScore()).to.equal("frame: 6,/ -- total: 24");
         });
         
-        it.skip('applies a spare extra points', () => {
+        it('applies a spare extra points', () => {
             bowling.roll(1);
             bowling.roll(4);
             bowling.roll(4);
             bowling.roll(5);
             expect(bowling.getScore()).to.equal("frame: 4,5 -- total: 14");
             forceSpare(6);
-            expect(bowling.getScore()).to.equal("frame: 6,/ -- total: 14");
+            expect(bowling.getScore()).to.equal("frame: 6,/ -- total: 24");
             bowling.roll(2);
-            expect(bowling.getScore()).to.equal("frame: 2,? -- total: 26");
+            expect(bowling.getScore()).to.equal("frame: 2,? -- total: 24");
             bowling.roll(3);
             expect(bowling.getScore()).to.equal("frame: 2,3 -- total: 31");
         });
@@ -85,22 +85,33 @@ describe("bowling game", () => {
             bowling.roll(4);
             bowling.roll(5);
             forceSpare(6);
+            expect(bowling.getScore()).to.equal("frame: 6,/ -- total: 24");
             forceSpare(5);
-            expect(bowling.getScore()).to.equal("frame: 5,/ -- total: 14");
+            expect(bowling.getScore()).to.equal("frame: 5,/ -- total: 39");
         });
     });
     
     describe("strike cases", () => {
         it('accounts for a strike frame', () => {
             bowling.roll(10);
-            expect(bowling.getScore()).to.equal("frame: X -- total: 0");
+            expect(bowling.getScore()).to.equal("frame: X -- total: 10");
         });
         
         it('accounts for a regular frame followed by a strike frame', () => {
             bowling.roll(2);
             bowling.roll(5);
             bowling.roll(10);
-            expect(bowling.getScore()).to.equal("frame: X -- total: 7");
+            expect(bowling.getScore()).to.equal("frame: X -- total: 17");
+        });
+        
+        it('accounts for a regular frame after a strike frame', () => {
+            bowling.roll(2);
+            bowling.roll(5);
+            bowling.roll(10);
+            expect(bowling.getScore()).to.equal("frame: X -- total: 17");
+            bowling.roll(1);
+            bowling.roll(8);
+            expect(bowling.getScore()).to.equal("frame: 1,8 -- total: 35");
         });
     });
 });
